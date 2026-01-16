7bad2f86c15b1461f0e1768da598aeee1f93032e
Mohamed Khayal <khayyalmohamed5@gmail.com>
2026-01-16 14:28:16 +0200

    Deployment

--- middlewares/cors.Handler.js
+++ middlewares/cors.Handler.js
@@ -8,11 +8,15 @@ const corsOptions = {
   origin(origin, callback) {
     if (!origin) return callback(null, true);
 
+    if (allowedOrigins.length === 0) {
+      return callback(null, true);
+    }
+
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
 
-    return callback(new Error("Not allowed by CORS"));
+    return callback(null, false);
   },
 
   credentials: true,
