6e2100c923db6bcf839a3a697030c998eb861c35
Mohamed Khayal <khayyalmohamed5@gmail.com>
2026-01-16 14:18:02 +0200

    Deployment

--- config/db.Config.js
+++ config/db.Config.js
@@ -3,15 +3,13 @@ const logger = require("../utilts/logger");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: process.env.DB_NAME,
-    });
-
-    logger.info(`MongoDB Connected To Atlas`);
+    logger.info("Connecting to MongoDB...");
+    await mongoose.connect(process.env.MONGO_URI);
+    logger.info("MongoDB Connected To Atlas");
   } catch (error) {
     logger.error("MongoDB connection failed");
-    logger.error(error.message);
-    throw error;
+    logger.error(error);
+    process.exit(1);
   }
 };
 
