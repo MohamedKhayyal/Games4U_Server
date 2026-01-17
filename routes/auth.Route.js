5efe48bf8dc1fcc60c9d391d84b6b29fb5376b05
Mohamed Khayal <khayyalmohamed5@gmail.com>
2026-01-17 14:52:41 +0200

    Deployment

--- routes/auth.Route.js
+++ routes/auth.Route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authController = require("../controllers/auth.Controller");
+const { uploadSingle, resize } = require("../middlewares/upload");
 const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
+router.post("/signup", uploadSingle("photo"), resize, authController.signup);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
 
@@ -12,7 +13,7 @@ router.post(
   "/create-admin",
   auth.protect,
   auth.restrictTo("admin"),
-  authController.createAdmin
+  authController.createAdmin,
 );
 
 module.exports = router;
