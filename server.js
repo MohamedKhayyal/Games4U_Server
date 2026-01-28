0517e01a1e081cc5693edaafb98fa6eec01e2209
Mohamed Khayal <khayyalmohamed5@gmail.com>
2026-01-28 19:13:38 +0200

    helth API

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
+});
+
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
