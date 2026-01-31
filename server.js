16d494f5439f83a4e7a4e3caaae2799829a8fc1a
Mohamed Khayal <khayyalmohamed5@gmail.com>
2026-01-31 18:13:32 +0200

    fix: downgrade express to v4 (stable)

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const adminRoute = require("./routes/admin.Route");
 const logsRoutes = require("./routes/logs.Route");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // GLOBAL MIDDLEWARES
 app.use(corsHandler);
