8638643732df3af1e48b1edf314f46dc09b996bb
Mohamed Khayal <khayyalmohamed5@gmail.com>
2026-01-31 20:30:08 +0200

    fix deployment

--- config/db.Config.js
+++ config/db.Config.js
@@ -2,21 +2,23 @@ const mongoose = require("mongoose");
 const logger = require("../utilts/logger");
 
 const connectDB = async () => {
-  try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined in environment variables");
-    }
+  if (!process.env.MONGO_URI) {
+    logger.error("MONGO_URI is not defined");
+    process.exit(1);
+  }
 
+  try {
     logger.info("Connecting to MongoDB...");
 
     await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, 
+      serverSelectionTimeoutMS: 5000,
     });
 
     logger.info("MongoDB Connected To Atlas");
   } catch (error) {
     logger.error("MongoDB connection failed");
     logger.error(error.message || error);
+    process.exit(1);
   }
 };
 
